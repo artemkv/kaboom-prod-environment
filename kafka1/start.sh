cc91def17ff9707bce74d06f03563904bb5abba3
artemkv <artemkv@gmail.com>
2019-01-30 23:51:21 +0100

    kafka start scripts

---
+++ kafka1/start.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+systemctl start kafka.service
+systemctl start zookeeper1.service
+systemctl start zookeeper2.service
---
+++ kafka2/start.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+systemctl start kafka.service
+systemctl start zookeeper3.service
+systemctl start zookeeper4.service
