22c76cc0743988d4a662fb1ae6ea41fee7505602
artemkv <artemkv@gmail.com>
2019-01-31 00:18:48 +0100

    removed 4th zookeeper

--- kafka2/configure.sh
+++ kafka2/configure.sh
@@ -5,22 +5,15 @@ cp ./server.properties /usr/local/bin/kafka/config/server.properties
 
 # Copy zookeeper1 configuration
 cp ./zookeeper3.properties /usr/local/bin/kafka/config/zookeeper3.properties
-cp ./zookeeper4.properties /usr/local/bin/kafka/config/zookeeper4.properties
 
 # Set up zookeeper3 id
 mkdir -p /tmp/zookeeper3
 echo "3" > /tmp/zookeeper3/myid
 
-# Set up zookeeper4 id
-mkdir -p /tmp/zookeeper4
-echo "4" > /tmp/zookeeper4/myid
-
 # Set up services
 cp ./kafka.service /etc/systemd/system/kafka.service
 cp ./zookeeper3.service /etc/systemd/system/zookeeper3.service
-cp ./zookeeper4.service /etc/systemd/system/zookeeper4.service
 
 systemctl daemon-reload
 systemctl enable kafka.service
 systemctl enable zookeeper3.service
-systemctl enable zookeeper4.service
--- kafka2/start.sh
+++ kafka2/start.sh
@@ -2,4 +2,3 @@
 
 systemctl start kafka.service
 systemctl start zookeeper3.service
-systemctl start zookeeper4.service
