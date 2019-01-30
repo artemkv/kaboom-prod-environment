7ae6fd83150de7d3ac9f98567fb031c68d2573ef
artemkv <artemkv@gmail.com>
2019-01-31 00:24:34 +0100

    reorder services

--- kafka1/start.sh
+++ kafka1/start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-systemctl start kafka.service
 systemctl start zookeeper1.service
 systemctl start zookeeper2.service
+systemctl start kafka.service
--- kafka2/start.sh
+++ kafka2/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-systemctl start kafka.service
 systemctl start zookeeper3.service
+systemctl start kafka.service
